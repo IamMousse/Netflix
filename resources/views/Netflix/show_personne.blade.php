279fd4d0009fdd7913d1e7def42fdbf5be0c4bb8
IamMousse <mme.roselynemartin@gmail.com>
2023-11-14 17:50:38 -0500

    wip netflix 2023-11-14
    modif film bug

--- resources/views/Netflix/show_personne.blade.php
+++ resources/views/Netflix/show_personne.blade.php
@@ -14,47 +14,47 @@
           <div class="col-xl-4">
             <img src="{{$personne->photo}}" alt="" width="500px">
           </div>
-          <div class="col-xl-4">
-            <h1>{{$personne->nom}}</h1>
-            <h3>{{$personne->dateN}}</h3>
-          <div>
-          
-            <a href="{{route('personnes.edit', [$personne])}}" class="btn btn-danger">
-                            Editer le film
-                  </a>
-          </div>
+          <div class="col-xl-4 text-center">
+            <div class="row">
+              <h1>{{$personne->nom}}</h1>
+              <h3>{{$personne->dateN}}</h3>
+              <br><br><br>
             </div>
+            <div class="row">
+              <a href="{{route('personnes.edit', [$personne])}}" class="btn btn-danger">
+                Editer le film
+              </a>
+            </div>
+            <div class="row">
+              @foreach ($personne->filmsRealises as $film)
+                <h1>Films réalisés</h1>
+                <a href="{{route('film.show', [$film])}}">
+                  <img src="{{$film->pochetteURL}}" alt="" width="300px" height="300px">
+                </a>
+                @endforeach
+
+                @foreach($personne->filmsProduits as $film)
+                <h1>Films produits</h1>
+                <a href="{{route('film.show', [$film])}}">
+                  <img src="{{$film->pochetteURL}}" alt="" width="300px" height="300px">
+                </a>
+                @endforeach
+
+                @foreach($personne->filmsActeurs as $film)
+                <h1>Films jouer</h1>
+                <a href="{{route('film.show', [$film])}}">
+                  <img src="{{$film->pochetteURL}}" alt="" width="300px" height="300px">
+                </a>
+                @endforeach
+            </div>
+
+            
+
             <div class="col-xl-2"></div>
         </div>
 
       </div>
-      <div class="row">
-        <div class="col-xl-6 text-center">
-          
-          @foreach ($personne->filmsRealises as $film)
-          <h1>Films réalisés</h1>
-          <a href="{{route('film.show', [$film])}}">
-            <img src="{{$film->pochetteURL}}" alt="" width="300px" height="300px">
-          </a>
-          @endforeach
-
-          
-          @foreach($personne->filmsProduits as $film)
-          <h1>Films produits</h1>
-          <a href="{{route('film.show', [$film])}}">
-            <img src="{{$film->pochetteURL}}" alt="" width="300px" height="300px">
-          </a>
-          @endforeach
-
-          
-          @foreach($personne->filmsActeurs as $film)
-          <h1>Films jouer</h1>
-          <a href="{{route('film.show', [$film])}}">
-            <img src="{{$film->pochetteURL}}" alt="" width="300px" height="300px">
-          </a>
-          @endforeach
-        </div>
-      </div>
+      
     </section>
 </div>
 
