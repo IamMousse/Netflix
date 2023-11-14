b93dd627dba265290bb2241696e8cc91ca08214d
IamMousse <mme.roselynemartin@gmail.com>
2023-11-13 22:16:50 -0500

    wip netflix 2023-11-13
    debug sur page personne/acteurs

--- resources/views/Netflix/personne.blade.php
+++ resources/views/Netflix/personne.blade.php
@@ -6,6 +6,22 @@
 
 <div class="wrapper">
   <section class="main-container" >
+    <div class="location" id="tous">
+      <h1 id="tous">Tous</h1>
+      <div class="box">
+      @if(count($personnes))
+        @foreach($personnes as $personne)
+        <a href="{{route('personne.show', [$personne])}}">
+          <img src="{{$personne->photo}}" alt="" width="250px">
+        </a>
+        @endforeach
+      @else
+        <p>Il n'y a pas de Personne</p>
+      @endif    
+    </div>
+  </section>
+
+  <section class="main-container" >
     <div class="location" id="realisateur">
       <h1 id="realisateur">Realisateur</h1>
       <div class="box">
@@ -38,18 +54,20 @@
   </section>
 
   <section class="main-container" >
-    <div class="location" id="tous">
-      <h1 id="tous">Tous</h1>
+    <div class="location" id="acteur">
+      <h1 id="acteur">Acteur</h1>
       <div class="box">
-      @if(count($personnes))
-        @foreach($personnes as $personne)
-        <a href="{{route('personne.show', [$personne])}}">
-          <img src="{{$personne->photo}}" alt="" width="250px">
-        </a>
+      @if(count($films))
+        @foreach($films as $film)
+          @foreach($film->personnes as $acteur)
+            <a href="{{route('personne.show', [$acteur])}}">
+              <img src="{{$acteur->photo}}" alt="" width="250px">
+            </a>
+          @endforeach
         @endforeach
       @else
         <p>Il n'y a pas de Personne</p>
-      @endif    
+      @endif   
     </div>
   </section>
 
