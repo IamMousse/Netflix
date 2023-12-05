c9b8f2f61704190fa5f917bf82278874e3ff35ec
IamMousse <mme.roselynemartin@gmail.com>
2023-12-05 17:43:07 -0500

    Update login.blade.php

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -9,13 +9,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body class="loginBG">
-<br><br><br><br>
+<br><br><br><br><br>
     <div class="container-fluid">
-        <br><br><br>
+        <br><br><br><br>
         <div class="row">
             <div class="col-xl-4"></div>
             <div class="col-xl-4 card bg-dark text-white text-center">
-                <div class="card-header bg-dark text-white mb-3"> <b> Connexion </b> </div>
+                <div class="card-header mb-3"> <b> Connexion </b> </div>
                 <div class="card-body text-white bg-secondary">
                     <form method="post" action="{{route('login')}}">
                         @csrf 
